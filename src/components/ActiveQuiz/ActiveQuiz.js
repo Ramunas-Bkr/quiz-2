2f4b00cb7e24120fedbfd3401b9f73b61b730b2f
Ramunas <rambek@gmail.com>
2020-06-17 17:31:48 +0300

    Answes list

--- src/components/ActiveQuiz/ActiveQuiz.js
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import classes from './ActiveQuiz.module.css'
+import AnswersList from './AnswersList/AnswersList'
 
-const ActiveQuiz = () => {
+const ActiveQuiz = (props) => {
     return (
         <div className={classes.ActiveQuiz}>
             <p className={classes.Question}>
@@ -12,12 +13,10 @@ const ActiveQuiz = () => {
                 <small>4 klausimas iš 12</small>
             </p>
 
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-                <li>4</li>
-            </ul>
+            <AnswersList 
+                answers={props.answers}
+
+            />
         </div>
     )
 }
